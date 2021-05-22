9bdde742af3075349e50bedb4a5f1ae0c667e054
baiama <primkulovbaiaman@gmail.com>
2021-05-23 01:00:08 +0600

    add colors

---
+++ lib/presentation/color_scheme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kColor2980B9 = Color(0xFF2980B9);
+const kColorBDBDBD = Color(0xFFBDBDBD);
+const kColorF6F6F6 = Color(0xFFF6F6F6);
+const kColorE8E8E8 = Color(0xFFE8E8E8);
+const kColorCC6666 = Color(0xFFCC6666);
+const kColor666666 = Color(0xFF666666);
+const kColor4D7EB7DC = Color(0x4D7EB7DC);
