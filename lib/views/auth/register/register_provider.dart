b6507ac1110ee5f627e2db5552f12d8392c4b4d0
baiama <primkulovbaiaman@gmail.com>
2021-05-24 20:06:37 +0600

    gg

--- lib/views/auth/register/register_provider.dart
+++ lib/views/auth/register/register_provider.dart
@@ -8,6 +8,37 @@ class RegisterProvider extends AuthProvider {
       notifyListeners();
       return;
     }
+    if (phone == null || phone!.length == 0) {
+      setError = Strings.errorEmpty + 'Телефон';
+      notifyListeners();
+      return;
+    }
+
+    if (!phone!.startsWith('+')) {
+      phone = '+' + phone!;
+    }
+
+    if (phone!.length != 12) {
+      setError = Strings.errorEmpty2 + 'Телефон';
+      notifyListeners();
+      return;
+    }
+
+    if (email == null || email!.length == 0) {
+      setError = Strings.errorEmpty + 'Email';
+      notifyListeners();
+      return;
+    }
+
+    if (!RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email!)) {
+      setError = Strings.errorEmpty2 + 'Email';
+      notifyListeners();
+      return;
+    }
+
+    setError = '';
 
     api.register(
         onSuccess: () {},
