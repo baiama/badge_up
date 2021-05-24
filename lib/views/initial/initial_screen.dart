61d287d4ba9c88f69da927d6c098d1132068d36b
baiama <primkulovbaiaman@gmail.com>
2021-05-25 02:13:34 +0600

    gg

--- lib/views/initial/initial_screen.dart
+++ lib/views/initial/initial_screen.dart
@@ -1,11 +1,42 @@
 import 'package:budge_up/presentation/button_styles.dart';
 import 'package:budge_up/presentation/color_scheme.dart';
 import 'package:budge_up/presentation/custom_icons.dart';
+import 'package:budge_up/utils/preference_helper.dart';
 import 'package:budge_up/views/auth/login/login_screen.dart';
 import 'package:budge_up/views/auth/register/register_screen.dart';
+import 'package:budge_up/views/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
-class InitialScreen extends StatelessWidget {
+class InitialScreen extends StatefulWidget {
+  @override
+  _InitialScreenState createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool isAuthorized = true;
+
+  @override
+  void initState() {
+    super.initState();
+    checkToken();
+  }
+
+  void checkToken() async {
+    String token = await PreferenceHelper().token;
+    Future.delayed(Duration(seconds: 4)).then((value) {
+      if (token.length > 0) {
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => HomeScreen()),
+            (route) => false);
+      } else {
+        setState(() {
+          isAuthorized = false;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,25 +54,27 @@ class InitialScreen extends StatelessWidget {
                   child: kLogoIcon,
                 ),
               ),
-              ElevatedButton(
-                style: kElevatedButtonStyle1,
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()));
-                },
-                child: Text('Вход'),
-              ),
+              if (!isAuthorized)
+                ElevatedButton(
+                  style: kElevatedButtonStyle1,
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
+                  },
+                  child: Text('Вход'),
+                ),
               SizedBox(height: 24),
-              ElevatedButton(
-                style: kElevatedButtonStyle2,
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => RegisterScreen()));
-                },
-                child: Text('Регистрация'),
-              ),
+              if (!isAuthorized)
+                ElevatedButton(
+                  style: kElevatedButtonStyle2,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => RegisterScreen()));
+                  },
+                  child: Text('Регистрация'),
+                ),
             ],
           ),
         ),
