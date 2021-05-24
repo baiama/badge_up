7a4ee340e4904b86c8b000b4c90415a25d94d757
baiama <primkulovbaiaman@gmail.com>
2021-05-24 20:32:05 +0600

    gg

--- lib/views/auth/login/login_screen.dart
+++ lib/views/auth/login/login_screen.dart
@@ -1,9 +1,22 @@
 import 'package:budge_up/presentation/text_styles.dart';
 import 'package:budge_up/presentation/widgets.dart';
+import 'package:budge_up/views/auth/login/login_provider.dart';
 import 'package:budge_up/views/auth/reset_password/reset_password.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<LoginProvider>(context, listen: false).setUp();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,56 +36,63 @@ class LoginScreen extends StatelessWidget {
             }
           }
         },
-        child: Container(
-          color: Colors.white,
-          padding: EdgeInsets.all(24),
-          child: Form(
-            autovalidateMode: AutovalidateMode.always,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: 25),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Телефон или Email *',
-                  ),
-                ),
-                SizedBox(height: 24),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Пароль *',
-                  ),
-                ),
-                SizedBox(height: 24),
-                Text(
-                  'Некорректное заполнение: Телефон',
-                  style: kInterReg16ColorCC6666,
-                  textAlign: TextAlign.center,
-                ),
-                Expanded(child: Container()),
-                ElevatedButton(onPressed: () {}, child: Text('Войти')),
-                SizedBox(height: 10),
-                GestureDetector(
-                  onTap: () async {
-                    final result = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ResetPasswordScreen()));
-                    print(result);
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 14),
-                    child: Text(
-                      'Я забыл пароль',
-                      style: kInterReg14Color2980B9,
-                      textAlign: TextAlign.center,
+        child: Consumer<LoginProvider>(
+          builder: (BuildContext context, provider, Widget? child) {
+            return Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(height: 25),
+                  TextFormField(
+                    onChanged: (value) {
+                      provider.email = value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Телефон или Email *',
                     ),
                   ),
-                ),
-                SizedBox(height: 10),
-              ],
-            ),
-          ),
+                  SizedBox(height: 24),
+                  TextFormField(
+                    onChanged: (value) {
+                      provider.password = value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Пароль *',
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  Text(
+                    provider.error,
+                    style: kInterReg16ColorCC6666,
+                    textAlign: TextAlign.center,
+                  ),
+                  Expanded(child: Container()),
+                  ElevatedButton(onPressed: () {}, child: Text('Войти')),
+                  SizedBox(height: 10),
+                  GestureDetector(
+                    onTap: () async {
+                      final result = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ResetPasswordScreen()));
+                      print(result);
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 14),
+                      child: Text(
+                        'Я забыл пароль',
+                        style: kInterReg14Color2980B9,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
