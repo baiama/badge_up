938a9ce742c7c5cec6867cc1f9bc1df517f68267
baiama <primkulovbaiaman@gmail.com>
2021-05-24 21:23:33 +0600

    gg

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:budge_up/views/garage/garage_screen.dart';
 import 'package:budge_up/views/park/park_screen.dart';
 import 'package:budge_up/views/parking_auto/parking_auto.dart';
+import 'package:budge_up/views/settings/settings_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,6 +15,8 @@ class _HomeScreenState extends State<HomeScreen> {
   final _screens = [
     ParkingAuto(),
     ParkScreen(),
+    GarageScreen(),
+    SettingsScreen(),
   ];
 
   @override
