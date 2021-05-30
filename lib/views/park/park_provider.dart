f5ce0b88f76f7afe326da117702a4258e7607bbf
baiama <primkulovbaiaman@gmail.com>
2021-05-30 23:09:58 +0600

    gg

---
+++ lib/views/park/park_provider.dart
@@ -0,0 +1,6 @@
+import 'package:budge_up/base/base_provider.dart';
+import 'package:flutter/material.dart';
+
+class ParkProvider extends BaseProvider {
+  void getItems() {}
+}
--- lib/views/parking_auto/parking_auto.dart
+++ lib/views/parking_auto/parking_auto.dart
@@ -1,13 +1,29 @@
+import 'package:budge_up/views/park/park_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ParkingAuto extends StatelessWidget {
+class ParkingAuto extends StatefulWidget {
   const ParkingAuto({Key? key}) : super(key: key);
 
+  @override
+  _ParkingAutoState createState() => _ParkingAutoState();
+}
+
+class _ParkingAutoState extends State<ParkingAuto> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Парковать авто'),
+      ),
+      body: Container(
+        child: Consumer<ParkProvider>(
+          builder: (context, provider, Widget? child) {
+            return ListView.builder(itemBuilder: (context, index) {
+              return Container();
+            });
+          },
+        ),
       ),
     );
   }
