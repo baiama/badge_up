1ea4993cca4154e42c215ed30f732496fcce96eb
baiama <primkulovbaiaman@gmail.com>
2021-06-02 00:54:38 +0600

    gg

--- lib/views/parking_auto/search_result.dart
+++ lib/views/parking_auto/search_result.dart
@@ -1,9 +1,14 @@
 import 'package:budge_up/presentation/widgets.dart';
 import 'package:flutter/material.dart';
 
-class SearchResult extends StatelessWidget {
+class SearchResult extends StatefulWidget {
   const SearchResult({Key? key}) : super(key: key);
 
+  @override
+  _SearchResultState createState() => _SearchResultState();
+}
+
+class _SearchResultState extends State<SearchResult> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
