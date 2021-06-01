a0ea5dff98fd1bba9ac53ec852478394818be08d
baiama <primkulovbaiaman@gmail.com>
2021-06-01 08:56:01 +0600

    gg

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:budge_up/views/auth/reset_password/reset_password_provider.dart'
 import 'package:budge_up/views/garage/garage_provider.dart';
 import 'package:budge_up/views/initial/initial_screen.dart';
 import 'package:budge_up/views/park/park_provider.dart';
+import 'package:budge_up/views/parking_auto/parking_auto_provider.dart';
 import 'package:budge_up/views/settings/settings_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => SettingsProvider()),
         ChangeNotifierProvider(create: (_) => ResetPasswordProvider()),
         ChangeNotifierProvider(create: (_) => ParkProvider()),
+        ChangeNotifierProvider(create: (_) => ParkingAutoProvider()),
         ChangeNotifierProvider(create: (_) => GarageProvider()),
       ],
       child: MaterialApp(
--- lib/views/parking_auto/parking_auto_provider.dart
+++ lib/views/parking_auto/parking_auto_provider.dart
@@ -1,1 +1,25 @@
+import 'package:budge_up/api/garage_api.dart';
+import 'package:budge_up/base/base_provider.dart';
+import 'package:budge_up/models/auto_model.dart';
 
+class ParkingAutoProvider extends BaseProvider {
+  GarageApi _api = GarageApi();
+  List<AutoModel>? _items;
+  List<AutoModel> get items => _items != null ? _items! : [];
+  void getItems() {
+    _items = [];
+    setIsRequestSend = false;
+    if (!isRequestSend) {
+      setIsRequestSend = true;
+      _api.getItems(
+        onSuccess: (value) {
+          _items = value;
+          setIsRequestSend = false;
+        },
+        onFailure: () {
+          setIsRequestSend = false;
+        },
+      );
+    }
+  }
+}
