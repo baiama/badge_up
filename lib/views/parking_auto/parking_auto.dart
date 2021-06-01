c4fdc2999390ef78a4b40e32f890331e1dcba6ab
baiama <primkulovbaiaman@gmail.com>
2021-06-01 09:43:33 +0600

    gg

--- lib/views/parking_auto/parking_auto.dart
+++ lib/views/parking_auto/parking_auto.dart
@@ -209,6 +209,43 @@ class _AutoListViewState extends State<AutoListView> {
                     Navigator.pop(context);
                   },
                   child: Text('Готово')),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GarageAddScreen()),
+                  ).then((value) {
+                    Provider.of<ParkingAutoProvider>(context, listen: false)
+                        .getItems();
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 22, vertical: 26),
+                  child: Text(
+                    'Добавить новый авто',
+                    style: kInterReg16ColorCC6666.copyWith(
+                      color: kColor2980B9,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  padding: EdgeInsets.only(left: 22, right: 22, bottom: 26),
+                  child: Text(
+                    'Отменить',
+                    style: kInterReg16ColorCC6666.copyWith(
+                      color: kColor2980B9,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
