b0787af678e2d1e22e528f16b0f4133294d4684c
baiama <primkulovbaiaman@gmail.com>
2021-06-01 08:03:33 +0600

    gg

--- lib/views/garage/garage_screen.dart
+++ lib/views/garage/garage_screen.dart
@@ -16,27 +16,44 @@ class _GarageScreenState extends State<GarageScreen> {
   @override
   void initState() {
     super.initState();
-    Provider.of<GarageProvider>(context, listen: false).getItems();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      Provider.of<GarageProvider>(context, listen: false).getItems();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Мой гараж'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => GarageAddScreen()),
-                );
-              },
-              icon: CustomIcon(
-                customIcon: CustomIcons.addNew,
-              ))
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Мой гараж'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GarageAddScreen()),
+                  );
+                },
+                icon: CustomIcon(
+                  customIcon: CustomIcons.addNew,
+                ))
+          ],
+        ),
+        body: Consumer<GarageProvider>(
+          builder: (context, provider, Widget? child) {
+            if (provider.isRequestSend) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return Container(
+              child: ListView.builder(
+                  itemCount: provider.items.length,
+                  itemBuilder: (context, index) {
+                    return Container();
+                  }),
+            );
+          },
+        ));
   }
 }
