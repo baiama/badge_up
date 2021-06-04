84b892f5d07f61f285009bf4a4ae47d4d056cc82
baiama <primkulovbaiaman@gmail.com>
2021-06-04 21:35:52 +0600

    gg

--- lib/views/park/park_screen.dart
+++ lib/views/park/park_screen.dart
@@ -188,7 +188,7 @@ class ParkItem extends StatelessWidget {
                     child: isLoading
                         ? CircularLoader()
                         : Text(parkModel.close.id > 0 &&
-                                parkModel.close.id != user.id
+                                parkModel.close.id == user.id
                             ? 'Я все равно уехал'
                             : 'Уехать')),
               SizedBox(height: 12),
@@ -204,7 +204,7 @@ class ParkItem extends StatelessWidget {
                     color: kColorB2CC6666,
                     borderRadius: BorderRadius.circular(8)),
                 child: Text(
-                  parkModel.close.userId == user.id
+                  parkModel.close.userId != user.id
                       ? 'Закрыт мной'
                       : 'Закрыл меня',
                   style: kInterReg12.copyWith(color: Colors.white),
