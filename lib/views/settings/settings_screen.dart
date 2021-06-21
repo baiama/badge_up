834ea140e3fd9c1b6d3ba0aac9366f218c55cd61
baiama <primkulovbaiaman@gmail.com>
2021-06-21 08:34:09 +0600

    gg

--- lib/views/settings/settings_screen.dart
+++ lib/views/settings/settings_screen.dart
@@ -1,13 +1,11 @@
-import 'package:budge_up/components/alerts.dart';
-import 'package:budge_up/presentation/color_scheme.dart';
 import 'package:budge_up/presentation/custom_icons.dart';
 import 'package:budge_up/presentation/text_styles.dart';
 import 'package:budge_up/presentation/widgets.dart';
-import 'package:budge_up/utils/full_screen.dart';
 import 'package:budge_up/utils/strings.dart';
 import 'package:budge_up/views/components/cutom_allerts.dart';
 import 'package:budge_up/views/initial/initial_screen.dart';
 import 'package:budge_up/views/settings/settings_provider.dart';
+import 'package:budge_up/views/settings/widgets/profile_image_container.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -80,189 +78,93 @@ class _SettingsScreenState extends State<SettingsScreen> {
               padding: EdgeInsets.all(24),
               child: Consumer<SettingsProvider>(
                 builder: (context, provider, Widget? child) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      SizedBox(height: 26),
-                      if (provider.image == null &&
-                          provider.user.photo.length == 0)
-                        Container(
-                          alignment: Alignment.center,
-                          child: GestureDetector(
-                            onTap: () {
-                              showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return ImageAlert(
-                                      onImageSelected: (image) {
-                                        provider.image = image;
-                                        provider.updateView();
-                                      },
-                                    );
-                                  });
-                            },
-                            child: Container(
-                              height: 120,
-                              width: 120,
-                              alignment: Alignment.center,
-                              child: CustomIcon(
-                                customIcon: CustomIcons.addPhoto,
-                              ),
-                              decoration: BoxDecoration(
-                                color: kColorF6F6F6,
-                                shape: BoxShape.circle,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: kColor26656565,
-                                    spreadRadius: 4,
-                                    blurRadius: 20,
-                                    offset: Offset(0, 4),
-                                  ),
-                                ],
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 4,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      if (provider.image != null ||
-                          provider.user.photo.length > 0)
-                        Container(
-                          alignment: Alignment.center,
-                          child: Stack(
-                            children: [
-                              GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => FullScreenImage(
-                                              provider.user.photo,
-                                              provider.image)));
-                                },
-                                child: Container(
-                                  height: 120,
-                                  width: 120,
-                                  decoration: BoxDecoration(
-                                    color: kColorF6F6F6,
-                                    shape: BoxShape.circle,
-                                    image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: provider.currentImage,
-                                    ),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: kColor26656565,
-                                        spreadRadius: 20,
-                                        blurRadius: 20,
-                                        offset: Offset(0, 4),
-                                      ),
-                                    ],
-                                    border: Border.all(
-                                      color: Colors.white,
-                                      width: 5,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                bottom: 0,
-                                right: 0,
-                                child: IconButton(
-                                  icon: CustomIcon(
-                                    color: Colors.grey,
-                                    customIcon: CustomIcons.addPhoto,
-                                  ),
-                                  onPressed: () {
-                                    showDialog(
-                                        context: context,
-                                        builder: (context) {
-                                          return ImageAlert(
-                                            onImageSelected: (image) {
-                                              provider.image = image;
-                                              provider.updateView();
-                                            },
-                                          );
-                                        });
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      SizedBox(height: 24),
-                      Container(
-                        alignment: Alignment.center,
-                        child: Text(
-                          provider.user.name,
-                          style: kInterSemiBold18.copyWith(color: Colors.black),
-                        ),
-                      ),
-                      SizedBox(height: 50),
-                      TextFormField(
-                        onChanged: (value) {
-                          provider.name = value;
-                        },
-                        decoration: InputDecoration(
-                          hintText: 'Имя *',
-                        ),
-                      ),
-                      SizedBox(height: 24),
-                      TextFormField(
-                        onChanged: (value) {
-                          provider.phone = value;
-                        },
-                        decoration: InputDecoration(
-                          hintText: 'Телефон *',
-                        ),
-                      ),
-                      SizedBox(height: 24),
-                      TextFormField(
-                        onChanged: (value) {
-                          provider.email = value;
-                        },
-                        decoration: InputDecoration(
-                          hintText: 'Email *',
-                        ),
-                      ),
-                      SizedBox(height: 24),
-                      TextFormField(
-                        onChanged: (value) {
-                          provider.password = value;
-                        },
-                        decoration: InputDecoration(
-                          hintText: 'Пароль *',
-                        ),
-                      ),
-                      SizedBox(height: 24),
-                      TextFormField(
-                        onChanged: (value) {
-                          provider.passwordRepeat = value;
-                        },
-                        decoration: InputDecoration(
-                          hintText: 'Пароль еще раз *',
-                        ),
-                      ),
-                      SizedBox(height: 24),
-                      Text(
-                        provider.error,
-                        style: kInterReg16ColorCC6666,
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(
-                        height: 130,
-                      ),
-                      ElevatedButton(
-                          onPressed: () {
-                            provider.updateProfile(onSuccess: () {});
+                  return Form(
+                    autovalidateMode: AutovalidateMode.always,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        ProfileImageContainer(
+                          avatar: provider.user.photo,
+                          image: provider.image,
+                          onImageSelected: (value) {
+                            provider.image = value;
+                            provider.updateView();
                           },
-                          child: provider.isLoading
-                              ? CircularLoader()
-                              : Text(Strings.save)),
-                      SizedBox(height: 10),
-                    ],
+                          imageProvider: provider.currentImage,
+                        ),
+                        SizedBox(height: 24),
+                        Container(
+                          alignment: Alignment.center,
+                          child: Text(
+                            provider.user.name,
+                            style:
+                                kInterSemiBold18.copyWith(color: Colors.black),
+                          ),
+                        ),
+                        SizedBox(height: 50),
+                        TextFormField(
+                          onChanged: (value) {
+                            provider.name = value;
+                          },
+                          decoration: InputDecoration(
+                            hintText: 'Имя *',
+                          ),
+                        ),
+                        SizedBox(height: 24),
+                        TextFormField(
+                          onChanged: (value) {
+                            provider.phone = value;
+                          },
+                          decoration: InputDecoration(
+                            hintText: 'Телефон *',
+                          ),
+                        ),
+                        SizedBox(height: 24),
+                        TextFormField(
+                          onChanged: (value) {
+                            provider.email = value;
+                          },
+                          decoration: InputDecoration(
+                            hintText: 'Email *',
+                          ),
+                        ),
+                        SizedBox(height: 24),
+                        TextFormField(
+                          onChanged: (value) {
+                            provider.password = value;
+                          },
+                          decoration: InputDecoration(
+                            hintText: 'Пароль *',
+                          ),
+                        ),
+                        SizedBox(height: 24),
+                        TextFormField(
+                          onChanged: (value) {
+                            provider.passwordRepeat = value;
+                          },
+                          decoration: InputDecoration(
+                            hintText: 'Пароль еще раз *',
+                          ),
+                        ),
+                        SizedBox(height: 24),
+                        Text(
+                          provider.error,
+                          style: kInterReg16ColorCC6666,
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 130,
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              provider.updateProfile(onSuccess: () {});
+                            },
+                            child: provider.isLoading
+                                ? CircularLoader()
+                                : Text(Strings.save)),
+                        SizedBox(height: 10),
+                      ],
+                    ),
                   );
                 },
               ),
---
+++ lib/views/settings/widgets/profile_image_container.dart
@@ -0,0 +1,133 @@
+import 'dart:io';
+
+import 'package:budge_up/components/alerts.dart';
+import 'package:budge_up/presentation/color_scheme.dart';
+import 'package:budge_up/presentation/custom_icons.dart';
+import 'package:budge_up/utils/full_screen.dart';
+import 'package:flutter/material.dart';
+
+class ProfileImageContainer extends StatelessWidget {
+  final File? image;
+  final String avatar;
+  final Function(File?) onImageSelected;
+  final ImageProvider imageProvider;
+  const ProfileImageContainer({
+    Key? key,
+    required this.image,
+    required this.avatar,
+    required this.onImageSelected,
+    required this.imageProvider,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        if (image == null && avatar.length == 0)
+          Container(
+            alignment: Alignment.center,
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return ImageAlert(
+                        onImageSelected: (image) {
+                          onImageSelected(image);
+                        },
+                      );
+                    });
+              },
+              child: Container(
+                height: 120,
+                width: 120,
+                alignment: Alignment.center,
+                child: CustomIcon(
+                  customIcon: CustomIcons.addPhoto,
+                ),
+                decoration: BoxDecoration(
+                  color: kColorF6F6F6,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: kColor26656565,
+                      spreadRadius: 4,
+                      blurRadius: 20,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 4,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        if (image != null || avatar.length > 0)
+          Container(
+            alignment: Alignment.center,
+            child: Stack(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                FullScreenImage(avatar, image)));
+                  },
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    decoration: BoxDecoration(
+                      color: kColorF6F6F6,
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: imageProvider,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: kColor26656565,
+                          spreadRadius: 20,
+                          blurRadius: 20,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 5,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: IconButton(
+                    icon: CustomIcon(
+                      color: Colors.grey,
+                      customIcon: CustomIcons.addPhoto,
+                    ),
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return ImageAlert(
+                              onImageSelected: (image) {
+                                onImageSelected(image);
+                              },
+                            );
+                          });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+      ],
+    );
+  }
+}
