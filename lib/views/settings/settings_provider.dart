b6aab4d548e6d07d5618394605ab1fdb91e95ada
baiama <primkulovbaiaman@gmail.com>
2021-07-06 03:58:10 +0600

    gg

--- lib/views/settings/settings_provider.dart
+++ lib/views/settings/settings_provider.dart
@@ -95,7 +95,7 @@ class SettingsProvider extends AuthProvider {
       _api.uploadAvatar(
         image: image!,
         onSuccess: () {
-          image = null;
+          // image = null;
           setIsLoading = false;
         },
         onFailure: (value) {
