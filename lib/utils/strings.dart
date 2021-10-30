d31e444e4aa2fe8f431d56a49d9c233de30a06a5
baiama <primkulovbaiaman@gmail.com>
2021-10-31 01:23:43 +0600

    dsad

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,5 +1,5 @@
 class Strings {
-  static const String errorEmpty = 'Пожалуйста заполните поле: ';
+  static const String errorEmpty = 'Пожалуйста, заполните поле: ';
   static const String errorEmpty2 = 'Некорректное заполнение: ';
   static const String errorEmpty3 = 'Ошибка сервера';
   static const String errorEmpty4 = 'Мы не можем отсканировать номер машины';
